29945611c11ab84914d07a3434d349a01e2f666d
Abishek Aranganathan <abisheka@avecsage.com>
2024-02-19 23:12:43 +0530

    CreateMatch

--- NetClubApi/MatchModule/MatchBusinessLogic.cs
+++ NetClubApi/MatchModule/MatchBusinessLogic.cs
@@ -1,6 +1,27 @@
-﻿namespace NetClubApi.MatchModel
+﻿using NetClubApi.ClubModule;
+using NetClubApi.Model;
+using NetClubApi.Model.ResponseModel;
+using Org.BouncyCastle.Utilities;
+
+namespace NetClubApi.MatchModule
 {
-    public class MatchBusinessLogic
+    public interface IMatchBusinessLogic
     {
+        public Task<string> CreateSchedule(MatchModel match);
+    }
+    public class MatchBusinessLogic : IMatchBusinessLogic
+    {
+        private readonly IMatchDataAccess _matchDataAccess;
+
+        public MatchBusinessLogic(IMatchDataAccess matchDataAccess)
+        {
+            _matchDataAccess = matchDataAccess;
+        }
+        public async Task<string> CreateSchedule(MatchModel match)
+        {
+            return await _matchDataAccess.createMatch(match);
+
+            //return await Integers(1);
+        }
     }
 }
--- NetClubApi/MatchModule/MatchDataAccess.cs
+++ NetClubApi/MatchModule/MatchDataAccess.cs
@@ -1,7 +1,37 @@
-﻿namespace NetClubApi.MatchModel
+﻿using Microsoft.Data.SqlClient;
+using NetClubApi.Helper;
+using NetClubApi.Model;
+
+namespace NetClubApi.MatchModule
 {
-    public interface MatchDataAccess
+    public interface IMatchDataAccess
     {
+        public Task<string> createMatch(MatchModel match);
         
+    }
+    public class MatchDataAccess : IMatchDataAccess
+    {
+        public Task<string> createMatch(MatchModel match)
+        {
+            try
+            {
+                using (SqlConnection myCon = sqlHelper.GetConnection())
+                {
+                    myCon.Open();
+                    string sql1 = $@"INSERT INTO [dbo].[match] (club_id, league_id, team1_id, team2_id,player1_id,player2_id,start_date,end_date,court_id)
+                                   VALUES ('{match.club_id}','{match.league_id}','{match.team1_id}',    '{match.team2_id}','{match.player1_id}','{match.player2_id}','{match.start_date}','{match.end_date}','{match.court_id}')";
+                     using (SqlCommand myCommand1 = new SqlCommand(sql1, myCon))
+                     {
+                         myCommand1.ExecuteNonQuery();
+                     }
+                        myCon.Close();
+                }
+                return Task.FromResult("Success");
+            }
+            catch (Exception ex)
+            {
+               return Task.FromResult("Failed"+ex.Message);
+            }
+        }
     }
 }
